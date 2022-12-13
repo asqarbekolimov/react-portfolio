9b3ce8f8caea02fbfc8b75305ab77987311d1a65
asqarbekolimov <mr.coderx.pro@gmail.com>
2022-12-13 20:18:36 +0500

    Resume component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import Features from './Component/Features/Features';
 import Header from './Component/Head/Header';
 import Home from './Component/Hero/Home';
 import Portfolio from './Component/Portfolio/Portfolio';
+import Resume from './Component/Resume/Resume';
 
 function App() {
   return (
     <>
       <Header />
       <Home />
-      <Features/>
-      <Portfolio/>
+      <Features />
+      <Portfolio />
+      <Resume />
     </>
   );
 }
---
+++ src/Component/Resume/Resume.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import Card from './Card';
+import ResumeApi from './ResumeApi';
+import './Resume.css';
+
+const Resume = () => {
+  return (
+    <>
+      <section className='Resume' id='resume'>
+        <div className='container top'>
+          <div className='heading text-center'>
+            <h4>2+ YEARS OF EXPRUCENCE</h4>
+            <h1>My Resume</h1>
+          </div>
+
+          <div className='content-section mtop d_flex'>
+            <div className='left'>
+              <div className='heading'>
+                <h4>2020-2021</h4>
+                <h1>Education IT PARK</h1>
+              </div>
+
+              <div className='content'>
+                {ResumeApi.map((val, id) => {
+                  if (val.category === 'education') {
+                    return (
+                      <Card key={id} title={val.title} year={val.year} rate={val.rate} desc={val.desc} />
+                    )
+                  }
+                })}
+
+              </div>
+            </div>
+
+            <div className='left'>
+              <div className='heading'>
+                <h4>2021-2022</h4>
+                <h1>Project Experience</h1>
+              </div>
+
+              <div className='content'>
+                {ResumeApi.map((val, id) => {
+                  if (val.category === 'experience') {
+                    return (
+                      <Card key={id} title={val.title} year={val.year} rate={val.rate} desc={val.desc} />
+                    )
+                  }
+                })}
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Resume;
